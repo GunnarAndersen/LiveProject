889bfdcdbd3d9529060993ce48030d530f9c83e3
Gunnar Andersen <45769452+GunnarAndersen@users.noreply.github.com>
2019-09-04 16:17:20 -0600

    Added files from C# live project
    Could not put the entirety of the code, just had to use snippets from previously obtained screenshots.

---
+++ Avatar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace ManagementPortal.Enums
+{
+    public enum Avatar
+    {
+        [Description("Unselected")] Unselected = 0,
+        [Description("Fox")] Fox,
+        [Description("Cat")] Cat,
+        [Description("Dog")] Dog,
+        [Description("Human")] Human,
+    }
+    public class GetImageInfo
+    {
+        public static void GetImageName()
+        {
+            string path = "ye";
+            Avatar avatar = Avatar.Unselected;
+
+            switch (avatar)
+            {
+                case Avatar.Fox:
+                    Console.WriteLine("One"); //this is all placeholder stuff, please feel free to change it in the future! 
+                    break;
+                case Avatar.Cat:
+                    Console.WriteLine("Two");
+                    break;
+                case Avatar.Dog:
+                    Console.WriteLine("Three");
+                    break;
+                case Avatar.Human:
+                    Console.WriteLine("Four");
+                    break;
+                default:
+                    Console.WriteLine("Unselected");
+                    break;
+            }
+        }
+    }
+   
+}
+
---
+++ readme.txt
@@ -0,0 +1,14 @@
+Avatar.cs - There is a user class which needed greater personalization. 
+I created the avatar.cs class and I also made a function for it to use enums to select pictures
+
+While it cannot be seen, there was also a "Contact" page, and a few other things, which had to be removed- I was tasked with removing those
+and I did. 
+
+A lot of the work that I did is copyrighted material so I cannot post it in its entirety, so I have included screenshots of my code. I have commented on what is happening in each respective screenshot.
+
+codeScreenshot1 - I changed this so that it would return to the dashboard view if there is no name for the user.
+codeScreenshot2 - This function stores chat messages into a database which is the user's mailbbox.
+codeScreenshot3 - This function instantiates a new Manager of type ApplicationUser and it displays their first name and last initial in a chat Modal.
+codeScreenshot4 - This setup the chat modal so that it displays correctly.
+codeScreenshot5 - This is a view that shows the chat modal that I set up. In this case we changed it so that it will display the username of the user, this time, being, "fluffydog".
+
